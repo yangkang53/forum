ca0e48fce058cf8458072d479610107e3ab1d71b
yangkang53 <i@yangkang.me>
2017-03-16 22:07:23 +0800

    update bootstrap & static folder

--- forum/urls.py
+++ forum/urls.py
@@ -13,18 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import django
 from django.conf.urls import url
 from django.contrib import admin
 import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
-
-
-urlpatterns = [
-    url(r'^admin/',admin.site.urls),
-    url(r'^$',views.index),
+    url(r'^static/(?P<path>.*)$', django.contrib.staticfiles.views.serve),
+    url(r'^$', views.index),
 ]
 
 
